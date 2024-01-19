7e567aa86cd1ecdb06bbb13338d2e4e065d01007
Eric Saboya <44400065+EricSaboya@users.noreply.github.com>
2024-01-19 16:20:38 +0000

    Update inversionsetup.py

--- rhime_with_openghg/rhime/inversionsetup.py
+++ rhime_with_openghg/rhime/inversionsetup.py
@@ -1,8 +1,4 @@
 # ****************************************************************************
-# Created: 7 Nov. 2022
-# Author: Eric Saboya, School of Geographical Sciences, University of Bristol
-# Contact: eric.saboya@bristol.ac.uk
-# ****************************************************************************
 # Originally createdby Luke Western (ACRG) and updated by Eric Saboya
 # 
 # About
@@ -16,7 +12,7 @@ import xarray as xr
 from openghg.retrieve import get_obs_surface
 
 def opends(fn):
-    '''
+    """
     Open a netcdf dataset with xarray
     -----------------------------------
     Args:
@@ -27,15 +23,14 @@ def opends(fn):
         xarray.Dataset: 
             netcdf file as  dataset 
     -----------------------------------
-    '''
+    """
     with xr.open_dataset(fn) as load:
         ds = load.load()
-
         return ds
 
 def addaveragingerror(fp_all, sites, species, start_date, end_date, meas_period,  
                       inlet=None, instrument=None):
-    '''
+    """
     Adds the variablility within the averaging period to the mole fraction error.
     -----------------------------------
     Args:
@@ -61,18 +56,18 @@ def addaveragingerror(fp_all, sites, species, start_date, end_date, meas_period,
         fp_all from input with averaging error added to the mole fraction
         error
     -----------------------------------
-    '''
+    """
     #Add variability in measurement averaging period to repeatability 
     for i, site in enumerate(sites):
-        get_obs = get_obs_surface(site=site,
-                                  species=species,
-                                  inlet=inlet[i],
-                                  instrument=instrument[i],
-                                  average=meas_period[i],
-                                  start_date=start_date,
-                                  end_date=end_date)
+        get_obs = get_obs_surface(site = site,
+                                  species = species,
+                                  inlet = inlet[i],
+                                  instrument = instrument[i],
+                                  average = meas_period[i],
+                                  start_date = start_date,
+                                  end_date = end_date)
 
-        sitedataerr=pd.DataFrame(get_obs.data.mf, index=get_obs.data.time.values)
+        sitedataerr = pd.DataFrame(get_obs.data.mf, index=get_obs.data.time.values)
 
         if min(sitedataerr.index) > pd.to_datetime(start_date):
             sitedataerr.loc[pd.to_datetime(start_date)]= \
@@ -83,9 +78,9 @@ def addaveragingerror(fp_all, sites, species, start_date, end_date, meas_period,
                 [np.nan for col in sitedataerr.columns]
         # Now sort to get everything in the right order
         sitedataerr = sitedataerr.sort_index()
-        if 'mf_variability' in fp_all[site.upper()]:
+        if "mf_variability" in fp_all[site.upper()]:
             fp_all[site.upper()].mf_variability.values = np.squeeze(np.sqrt(fp_all[site.upper()].mf_variability.values**2 + sitedataerr.resample(meas_period[i]).std(ddof=0).dropna().values.T**2))
-        elif 'mf_repeatability' in fp_all[site]:
+        elif "mf_repeatability" in fp_all[site]:
             fp_all[site.upper()].mf_repeatability.values = np.squeeze(np.sqrt(fp_all[site.upper()].mf_repeatability.values**2 + sitedataerr.resample(meas_period[i]).std(ddof=0).dropna().values.T**2))
         else:
             print('No mole fraction error information available in {}.'.format('fp_all'+str([site])))
@@ -93,7 +88,7 @@ def addaveragingerror(fp_all, sites, species, start_date, end_date, meas_period,
     return fp_all
 
 def monthly_bcs(start_date, end_date, site, fp_data):
-    '''
+    """
     Creates a sensitivity matrix (H-matrix) for the boundary 
     conditions, which will map monthly boundary condition 
     scalings to the observations. This is for a single site.
@@ -112,13 +107,13 @@ def monthly_bcs(start_date, end_date, site, fp_data):
       hmbc (array):
         Sensitivity matrix by month for observations
     -----------------------------------        
-    '''
-    allmonth= pd.date_range(start_date, end_date, freq="MS")[:-1] 
+    """
+    allmonth = pd.date_range(start_date, end_date, freq = "MS")[:-1] 
     nmonth = len(allmonth)
     curtime = pd.to_datetime(fp_data[site].time.values).to_period("M")
     pmonth = pd.to_datetime(fp_data[site].resample(time="MS").mean().time.values)
     hmbc = np.zeros((4*nmonth, len(fp_data[site].time.values)))
-    count=0
+    count = 0
     for cord in range(4):
         for m in range(0,nmonth):
             if allmonth[m] not in pmonth:
@@ -133,7 +128,7 @@ def monthly_bcs(start_date, end_date, site, fp_data):
     return hmbc
 
 def create_bc_sensitivity(start_date, end_date, site, fp_data, freq):
-    '''
+    """
     Creates a sensitivity matrix (H-matrix) for the boundary 
     conditions, which will map boundary condition scalings to 
     the observations. This is for a single site. The frequency 
@@ -158,9 +153,9 @@ def create_bc_sensitivity(start_date, end_date, site, fp_data, freq):
       hmbc (array):
         Sensitivity matrix by for observations to boundary conditions
     -----------------------------------        
-    '''
+    """
     dys = int("".join([s for s in freq if s.isdigit()]))
-    alldates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date)+pd.DateOffset(days=dys), freq=freq)
+    alldates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date)+pd.DateOffset(days = dys), freq = freq)
     ndates = np.sum(alldates < pd.to_datetime(end_date))
     curdates = fp_data[site].time.values
     hmbc = np.zeros((4*ndates, len(fp_data[site].time.values)))
@@ -177,7 +172,7 @@ def create_bc_sensitivity(start_date, end_date, site, fp_data, freq):
     return hmbc
 
 def sigma_freq_indicies(ytime, sigma_freq):
-    '''
+    """
     Create an index that splits times 
     into given periods
     -----------------------------------
@@ -192,7 +187,7 @@ def sigma_freq_indicies(ytime, sigma_freq):
       output (array):
         index array that defines periods against time
     -----------------------------------
-    ''' 
+    """
     ydt = pd.to_datetime(ytime)
     output = np.zeros(shape = len(ytime)).astype(int)
     if sigma_freq == None:
@@ -222,7 +217,7 @@ def sigma_freq_indicies(ytime, sigma_freq):
     return output
         
 def offset_matrix(siteindicator):
-    '''
+    """
     Set up a matrix that can be used to add an offset to each site.
     This will anchor to the first site (i.e. first site has no offset)
     -----------------------------------
@@ -231,7 +226,7 @@ def offset_matrix(siteindicator):
         Array of values used for indicating the indices associated
         with each site used in the inversion
     -----------------------------------   
-    '''
+    """
     b = np.zeros((int(len(siteindicator)), int(max(siteindicator))+1))
     for i in range(int(max(siteindicator)+1)):
         b[siteindicator == i,i] = 1. 
