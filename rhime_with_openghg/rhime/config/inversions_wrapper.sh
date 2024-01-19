84a1b7d0c0a27761795929207a25fc6650d1ef20
Eric Saboya <44400065+EricSaboya@users.noreply.github.com>
2024-01-19 15:54:55 +0000

    Update inversions_wrapper.sh

--- rhime_with_openghg/rhime/config/inversions_wrapper.sh
+++ rhime_with_openghg/rhime/config/inversions_wrapper.sh
@@ -3,9 +3,9 @@
 # Wrapper script for submitting jobs on ACRC HPC
 # docs: https://www.acrc.bris.ac.uk/protected/hpc-docs/index.html
 # ****************************************************************************
-#SBATCH --job-name=inv_eg
-#SBATCH --output=openghg_inversions_eg.out
-#SBATCH --error=openghg_inversions_eg.err
+#SBATCH --job-name=rhime_eg
+#SBATCH --output=inversions_eg.out
+#SBATCH --error=inversions_eg.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
@@ -16,17 +16,12 @@
 
 # Set up Python environment
 module load lang/python/anaconda
-#source /sw/lang/anaconda.3.10.4-2021-11-fencis/bin/activate openghg_ed
-
-source /sw/lang/anaconda.3.10.4-2021-11-fencis/bin/activate  pymc_env
+source /sw/lang/anaconda.3.10.4-2021-11-fencis/bin/activate  rhime_openghg
 
 #conda info
 
 cd /user/home/wz22079/my_openghg/openghg
 echo $PWD
-echo 'Begin Inversion'
+echo "Begin Inversion"
 
-python //user/home/wz22079/my_openghg/openghg_inversions/openghg_inversions/hbmcmc/run_hbmcmc.py -c /user/home/wz22079/my_openghg/openghg_inversions/openghg_inversions/hbmcmc/config/openghg_hbmcmc_input_template_example.ini
-
-
-
+python ~/rhime_with_openghg/rhime_with_openghg/rhime/run_rhime.py -c ~/rhime_with_openghg/rhime_with_openghg/rhime/config/rhime_input_template_example.ini
